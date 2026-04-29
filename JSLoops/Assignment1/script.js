1286e4cb972d801032fcb85837a20d6e7db0cd14
rajkumarvns <rajkumar966601@gmail.com>
2026-04-29 22:34:27 +0530

    feat: add initial HTML and JavaScript files for assignment loops demonstration

---
+++ JSLoops/Assignment1/script.js
@@ -0,0 +1,92 @@
+/*
+.. Write a program to print the table of 15 up to 5 
+steps using a for loop. 
+*/
+console.log("--------------Q------------------");
+for(var i = 1; i<=5;i++)
+{
+    console.log(i*15);
+}
+
+/*
+.. Write a loop that starts from 2 and increases by 
+2 each time till 10. Explain why it prints only 
+even numbers. 
+*/
+console.log("--------------Q------------------");
+for(var x = 1; x<=10; x++)
+{
+    console.log(x*2);
+}
+
+/*
+1.. Print numbers from 1 to 5 using a for loop. 
+*/
+console.log("--------------Q1------------------");
+for(var a = 1; a<=5; a++)
+{
+    console.log(a);
+}
+/*
+2. Create a loop and explain how initialization, 
+condition, and increment work together step
+by-step.  
+*/
+console.log("--------------Q2------------------");
+for(var b = 1; b<=10; b++)
+{
+    console.log(b);
+}
+//3  Print numbers from 10 to 1 (reverse counting). 
+console.log("--------------Q3------------------");
+for(var c = 10; c>=1; c--)
+{
+    console.log(c);
+}
+//4 Print even numbers from 2 to 10. 
+console.log("--------------Q4------------------");
+for(var d = 1; d<=10; d++)
+{
+    console.log(d*2);
+}
+// 5  Print odd numbers from 1 to 9.
+console.log("--------------Q5------------------");  
+for(var e = 1; e<=9; e+2)
+{
+    console.log(e);
+}
+// 6  Print the table of 15 up to 10 steps.  
+console.log("--------------Q6------------------");  
+for(var f = 1; f<=10; f++)
+{
+    console.log(f*15);
+}
+
+// 7  Print numbers from 1 to 20 with increment of 2.    
+console.log("--------------Q7------------------");  
+for(var g = 1; g<=20; g+2)
+{
+    console.log(g);
+}
+// 8  Print numbers from 5 to 50 with increment of 5.      
+console.log("--------------Q8------------------");  
+for(var h = 5; h<=50; h+5)
+{
+    console.log(h);
+}
+
+/* 9  Create a loop and print the square of numbers 
+from 1 to 10. */      
+console.log("--------------Q9------------------");  
+for(var h = 1; h<=10; h++)
+{
+    console.log(h*h);
+}
+
+/* 10  Print numbers from 1 to 10 that are divisible 
+by 3.   */      
+console.log("--------------Q10------------------");  
+for(var i = 1; i<=10; i++)
+{
+    console.log(i/3);
+}
