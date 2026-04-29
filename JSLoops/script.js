f768483ae9e6acad388851b9d48951f1b4b21f06
rajkumarvns <rajkumar966601@gmail.com>
2026-04-29 19:33:24 +0530

    feat: add initial HTML and JavaScript files for loops demonstration

---
+++ JSLoops/script.js
@@ -0,0 +1,26 @@
+// Table of 2
+for (var i = 1; i <= 10; i++)
+{
+    console.log(i*2);
+}
+    // Iterate from 1 to 10 number
+let x = [24,58,10,34,65,34,21,76,86,99];
+for(var i = 0; i<10 ; i++)
+{
+    console.log(x[i]);
+}
+
+// while loop
+
+let i = 1;
+while(i<=10)
+{
+    console.log(i*2);
+    i++;
+}
+// do while loop
+var i = 1;
+do{
+    console.log(i*2);
+    i++;
+}while(i<=10);
