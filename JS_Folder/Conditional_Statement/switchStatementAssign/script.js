5e05dd21b43ec9d99e70a6a820ec6085a26527ab
rajkumarvns <rajkumar966601@gmail.com>
2026-05-05 21:03:11 +0530

    feat: add HTML and JavaScript files for switch statement examples

---
+++ JS_Folder/Conditional_Statement/switchStatementAssign/script.js
@@ -0,0 +1,75 @@
+// Q1
+let choice = 2;
+switch (choice) {
+  case 1: {
+    console.log("Tea");
+    break;
+  }
+  case 2: {
+    console.log("Coffee");
+    break;
+  }
+  case 3: {
+    console.log("Regular Tea");
+    break;
+  }
+  default: {
+    console.log("Water");
+  }
+}
+
+// Q2
+let days = 7;
+switch (days) {
+  case 1: {
+    console.log("Monday");
+    break;
+  }
+  case 2: {
+    console.log("Tuesday");
+    break;
+  }
+  case 3: {
+    console.log("Wednesday");
+    break;
+  }
+  case 4: {
+    console.log("Thursday");
+    break;
+  }
+  case 5: {
+    console.log("Friday");
+    break;
+  }
+  case 6: {
+    console.log("Saturday");
+    break;
+  }
+  case 7: {
+    console.log("Sunday");
+    break;
+  }
+  default: {
+    console.log("Invalid Day");
+  }
+}
+// Q3
+let choco = 1;
+switch (choco) {
+  case 1: {
+    console.log("Vanilla");
+    break;
+  }
+  case 2: {
+    console.log("Chocolate");
+    break;
+  }
+  case 3: {
+    console.log("Strawberry");
+    break;
+  }
+  default: {
+    console.log("Out of Stock!");
+  }
+}
+// 
