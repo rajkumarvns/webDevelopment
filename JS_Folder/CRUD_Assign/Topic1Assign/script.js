5e3311b3d84d41fd0a75cda5404864ce98554d0a
rajkumarvns <rajkumar966601@gmail.com>
2026-05-06 17:19:03 +0530

    feat: add HTML and JavaScript files for CRUD assignment structure

---
+++ JS_Folder/CRUD_Assign/Topic1Assign/script.js
@@ -0,0 +1,51 @@
+// Q1
+arr = [];
+console.log(arr);
+arr.push("user1");
+arr.push("user2");
+console.log(arr);
+
+// Q2
+users = ["Raj","Raunit","Prasoon","Anit","Ayush"];
+console.log(users);
+
+//Q3 Access first user
+console.log(users.at(4));
+
+// Q4 find user 
+// console.log(users.find(2));
+
+// Q5 find 
+
+// Q6 Update from Anit ot ashok
+users.splice(3,1,"Ashok");
+// users.splice(0,1,"RICR");
+console.log(users);
+
+// Q7 remove one user
+// users.filter("Ashok");
+// console.log(users);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
