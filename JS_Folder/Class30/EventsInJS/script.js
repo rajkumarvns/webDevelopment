a5a1a1203c3b6a76cf485de724897dee45e76753
rajkumarvns <rajkumar966601@gmail.com>
2026-05-08 00:24:57 +0530

    feat: add initial HTML, CSS, and JavaScript files for event handling examples

---
+++ JS_Folder/Class30/EventsInJS/script.js
@@ -0,0 +1,31 @@
+let btn1 = document.querySelector("#btn1");
+
+btn1.onclick = () =>{
+    console.log("btn1 was clicked!");
+    
+}
+
+let div = document.querySelector("div");
+div.onmouseover = () => {
+    console.log("You are inside the div");
+    
+}
+
+btn1.addEventListener("click",() =>{
+   console.log("button was clicked - handler1");
+    
+});
+btn1.addEventListener("click",() =>{
+   console.log("button was clicked - handler2");
+    
+});
+const handler3 = () =>{
+   console.log("button was clicked - handler3");
+    
+}
+btn1.addEventListener("click", handler3);
+btn1.addEventListener("click",() =>{
+   console.log("button was clicked - handler4");
+    
+});
+btn1.removeEventListener("click", handler3);
