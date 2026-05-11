9c8191cfdb22131b8059aeb3bd508943bb965ece
rajkumarvns <rajkumar966601@gmail.com>
2026-05-11 15:36:38 +0530

    feat: refine Calculator layout and enhance JavaScript functionality

--- Projects/Calculator/script.js
+++ Projects/Calculator/script.js
@@ -1,33 +1,29 @@
-function Calculate(data){
-    const text = document.getElementById("text")
-    if(data === "AC")
-    {
-        text.innerText = "0";
-        return;
-    }
-    if(data === "="){
-        const EdgeCase = text.innerText
-        const EQ = text.innerText;
-        const solution = eval(EQ);
-        text.innerText = solution;
-        return;
-    }
-    if(data === "C")
-    {
-        // const eq = equation.innerText;
-        text.innerText = text.innerText.slice(0,-1);
-        return;
-    }
-    if(text.innerText === "0"){
-        text.innerText + data;
-    }
-    else{
-
-        text.innerText = text.innerText + data;
-    }
-    document
-  .getElementById("keydown")
-  .addEventListener("keydown", function (event) {
-  });
-
+function Calculate(data) {
+  const text = document.getElementById("text");
+  if (data === "AC") {
+    text.innerText = "0";
+    return;
+  }
+  if (data === "=") {
+    const EdgeCase = text.innerText;
+    const EQ = text.innerText;
+    const solution = eval(EQ);
+    text.innerText = solution;
+    return;
+  }
+  if (data === "C") {
+    // const eq = equation.innerText;
+    text.innerText = text.innerText.slice(0, -1);
+    return;
+  }
+  if (text.innerText === "0") {
+    text.innerText = data;
+  } else {
+    text.innerText = text.innerText + data;
+  }
+  document
+    .getElementById("keypress")
+    .addEventListener("keypress", function (event) {
+      // console.log(event.key);
+    });
 }
