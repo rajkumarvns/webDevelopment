5c6da23414f324562294561fef9059673477c3b5
rajkumarvns <rajkumar966601@gmail.com>
2026-05-19 10:57:02 +0530

    feat: add initial HTML and JavaScript files for execution context practice

---
+++ JS_Folder/AdvanceJs/Execution_Context/script.js
@@ -0,0 +1,45 @@
+var x = 5;
+function show(){
+    var y = 10;
+    console.log(x+y);
+    
+}
+show();
+
+console.log(a);
+var a = 10;
+
+const obj = {
+    name: "Raj Gupta",
+    show: function(){
+        console.log(this.name);
+        
+    }
+};
+obj.show();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
