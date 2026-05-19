966c3a11ab82f9c91ae28be74d10c6802c5ad0a1
rajkumarvns <rajkumar966601@gmail.com>
2026-05-19 20:07:15 +0530

    feat: update index.html and script2.js for promise implementation and improved readability

--- JS_Folder/AdvanceJs/script2.js
+++ JS_Folder/AdvanceJs/script2.js
@@ -1,19 +1,46 @@
-function getData(value, nextData) {
-  setTimeout(() => {
-    console.log("Getting Data", value);
+// function getData(value, nextData) {
+//   setTimeout(() => {
+//     console.log("Getting Data", value);
 
-    if (nextData) {
-      nextData();
-    }
-  }, 3000);
+//     if (nextData) {
+//       nextData();
+//     }
+//   }, 3000);
+// }
+
+// getData(1, () => {
+//   getData(2, () => {
+//     getData(3, () => {
+//       getData(4, () => {
+//         getData(5);
+//       });
+//     });
+//   });
+// });
+
+// Promise
+function getData(value) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (value === 4) {
+        reject("Query Rejected");
+      } else {
+        console.log("Data Sent", value);
+        resolve("Query Solved");
+      }
+    }, 1000);
+  });
 }
 
-getData(1, () => {
-  getData(2, () => {
-    getData(3, () => {
-      getData(4, () => {
-        getData(5);
-      });
-    });
-  });
-});
+// getData(1)
+//   .then(() => getData(2))
+//   .then(() => getData(3))
+//   .then(() => getData(4))
+//   .catch((error) => console.log(error));
+
+// or but if we use await so complexity reduce like (n)^n to (1).
+await getData(1);
+await getData(2);
+await getData(3);
+await getData(4);
+await getData(5);
