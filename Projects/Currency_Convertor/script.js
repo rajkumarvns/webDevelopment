d0d1231d942111a25480b147c9e2cfd63351fdca
rajkumarvns <rajkumar966601@gmail.com>
2026-05-25 20:43:33 +0530

    feat: add Currency Convertor application with HTML, CSS, and JavaScript

---
+++ Projects/Currency_Convertor/script.js
@@ -0,0 +1,14 @@
+// const API_KEY = "416a8a92dfbdebae722dafca";
+
+// const countryName = [
+//   "India",
+//   "United States",
+//   "United Kingdom",
+//   "Canada",
+//   "Australia",
+//   "Germany",
+//   "Japan",
+//   "Brazil",
+//   "France",
+//   "South Africa",
+// ];
