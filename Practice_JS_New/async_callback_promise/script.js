a5524e2220c846416919061e3c3b8108ade9443e
rajkumarvns <rajkumar966601@gmail.com>
2026-05-25 13:04:12 +0530

    feat: implement async functions with promises for data fetching

--- Practice_JS_New/async_callback_promise/script.js
+++ Practice_JS_New/async_callback_promise/script.js
@@ -23,21 +23,42 @@ function getData(dataId) {
   }, 2000);
 }
 
+const getPromise = () => {
+  return new Promise((resolve, reject) => {
+    console.log("I am a promise");
+    // resolve("success");
+    reject("error");
+  });
+};
+let promise = getPromise();
+promise.then(() => {
+  console.log("promise Fulfilled");
+});
+promise.catch(() => {
+  console.log("rejected");
+});
 
+function asyncFunc1() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("some data1......");
+      resolve("success");
+    }, 4000);
+  });
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function asyncFunc2() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("some data2.....");
+      resolve("success");
+    }, 6000);
+  });
+}
+console.log("fetching data1....");
+let p1 = asyncFunc1();
+p1.then((res) => {
+  console.log("fetching data2....");
+  let p2 = asyncFunc2();
+  p2.then((res) => {});
+});
