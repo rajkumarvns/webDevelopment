fa82d28162908b6e566551d981343499372f2cd3
rajkumarvns <rajkumar966601@gmail.com>
2026-05-27 00:56:22 +0530

    feat: add initial HTML and JavaScript files for variable and data types demonstration

---
+++ Practice_JS_New/Again_Practice_JS/Class2/script.js
@@ -0,0 +1,18 @@
+// data types
+let a = 9007199254740991n;
+
+a = a + 2n;
+
+console.log(a);
+
+// array
+let x = [1, 2, 3];
+let b = x;
+b.pop();
+console.log(b);
+
+let v = {
+  name: "Raj",
+  age: 24,
+  gmail: "raj@123",
+};
