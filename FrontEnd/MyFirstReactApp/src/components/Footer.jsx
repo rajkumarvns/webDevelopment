9058d81e5c5f9a1df932bb11d85336e487b92462
rajkumarvns <rajkumar966601@gmail.com>
2026-05-31 10:55:18 +0530

    feat: add Bootstrap integration and restructure App component with Header and Footer

---
+++ FrontEnd/MyFirstReactApp/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer() {
+  return (
+    <>
+      <div id="footer">this is Footer</div>
+    </>
+  );
+}
+export default Footer;
---
+++ FrontEnd/MyFirstReactApp/src/components/Header.jsx
@@ -0,0 +1,8 @@
+function Header() {
+  return (
+    <>
+      <div id="header">This is Header</div>
+    </>
+  );
+}
+export default Header;
