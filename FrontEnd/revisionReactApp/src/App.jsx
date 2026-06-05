a30acf97b6f7c344482eab181e1bc009a5b590da
rajkumarvns <rajkumar966601@gmail.com>
2026-06-06 01:24:38 +0530

    feat: add lucide-react dependency and implement card component in App with updated styling

--- FrontEnd/revisionReactApp/src/App.jsx
+++ FrontEnd/revisionReactApp/src/App.jsx
@@ -1,13 +1,38 @@
 import { FaWifi } from "react-icons/fa";
+import { Bookmark } from "lucide-react";
 function App() {
   return (
     <>
-      <div className="bg-success p-3 rounded-2" id="div1">
-        This is practice div
+      <div className="parent">
+        <div className="card rounded-4">
+          <div className="top">
+            <img
+              src="https://thumbs.dreamstime.com/b/icons-sample-psost-setting-428651108.jpg"
+              alt="amazon logo"
+            />
+            <button>
+              Save <Bookmark size={15} />
+            </button>
+          </div>
+          <div className="center">
+            <h3>
+              Amazon <span>5 days ago</span>
+            </h3>
+            <h2>UI/UX Design</h2>
+            <div>
+              <h4>Part Time</h4>
+              <h4>Beginner Level</h4>
+            </div>
+          </div>
+          <div className="bottom">
+            <div>
+              <h3>$120/hr</h3>
+              <p>Mumbai, India</p>
+            </div>
+            <button>Apply Now</button>
+          </div>
+        </div>
       </div>
-      <button className="bg-success rounded-2 m-4 fs-4 d-flex align-items-center gap-2">
-        {<FaWifi />} <span>Save files</span>
-      </button>
     </>
   );
 }
