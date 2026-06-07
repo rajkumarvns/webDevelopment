034e7b5a7d794fcb29a96611b1174e0b6dd93bc7
rajkumarvns <rajkumar966601@gmail.com>
2026-06-08 00:30:40 +0530

    feat: implement digital clock component with real-time updates and styling

--- FrontEnd/revisionReactApp/src/App.jsx
+++ FrontEnd/revisionReactApp/src/App.jsx
@@ -1,8 +1,45 @@
 import { FaWifi } from "react-icons/fa";
 import { Bookmark } from "lucide-react";
+import { useState, useEffect } from "react";
 function App() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
   return (
     <>
+      <div
+        style={{
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          background: "#0f172a",
+        }}
+      >
+        <div
+          style={{
+            textAlign: "center",
+            color: "white",
+            padding: "30px",
+            borderRadius: "15px",
+            boxShadow: "0 0 20px rgba(255,255,255,0.9)",
+          }}
+        >
+          <h2>🕒 Digital Clock</h2>
+
+          <h1 style={{ fontSize: "60px" }} className="text-info">
+            {time.toLocaleTimeString()}
+          </h1>
+
+          <h3>{time.toDateString()}</h3>
+        </div>
+      </div>
       <div className="parent">
         <div className="card rounded-4">
           <div className="top">
