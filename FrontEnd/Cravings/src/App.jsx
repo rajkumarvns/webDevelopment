bcef85128077cc7994c8522a0641e8620c4d9af6
rajkumarvns <rajkumar966601@gmail.com>
2026-06-09 00:27:53 +0530

    feat: refine layout and styling of App component, adjust button padding and add content section

--- FrontEnd/Cravings/src/App.jsx
+++ FrontEnd/Cravings/src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
           </div>
 
           <div className="flex gap-5 ">
-            <div className="border-2 rounded-2xl flex items-center py-2 px-3">
+            <div className="border-2 rounded-2xl flex items-center  px-2">
               Login
             </div>
             <div className="border-2 rounded-lg flex items-center p-2">
@@ -21,6 +21,16 @@ const App = () => {
           <button className="hover:bg-amber-700 active:bg-blue-500 w-2xl my-3 p-3 text-white bg-amber-600  rounded-3xl">
             submit
           </button>
+        </div>
+        <div className=" flex content-center items-center  my-40 border-2 p-15 bg-white/20 rounded-3xl ">
+          <p className=" flex content-center items-center text-green-400">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima
+            natus vero sint impedit dicta, aspernatur architecto fuga cumque,
+            debitis molestiae labore! Ipsum esse dicta sed deserunt eveniet vero
+            voluptatibus itaque? Lorem ipsum dolor, sit amet consectetur
+            adipisicing elit. Aliquid laudantium cum numquam? Odit sit sed
+            veniam doloribus, aliquid tempora dignissimos?
+          </p>
         </div>
       </div>
     </>
