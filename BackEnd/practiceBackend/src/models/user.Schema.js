3d107a1b4d793fa033ce0129042c4bba49f066c0
rajkumarvns <rajkumar966601@gmail.com>
2026-06-24 10:36:21 +0530

    refactor: update UserSchema definition for consistency and clarity

--- BackEnd/practiceBackend/src/models/user.Schema.js
+++ BackEnd/practiceBackend/src/models/user.Schema.js
@@ -1,43 +1,53 @@
 import mongoose from "mongoose";
-const UserSchema = mongoose.Schema(
-  {
-    fullName: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    phone: {
-      type: String,
-      required: true,
-    },
-    gender: {
-      type: String,
-      required: true,
-    },
-    dob: {
-      type: String,
-      required: true,
-    },
-    photo: {
-      url: {
-        type: String,
-      },
-      publicID: {
-        type: String,
-      },
-    },
+
+const UserSchema = new mongoose.Schema(
+{
+  fullName:{
+    type:String,
+    required:true
   },
-  {
-    timestamp: true,
+
+  email:{
+    type:String,
+    required:true,
+    unique:true
   },
+
+  password:{
+    type:String,
+    required:true
+  },
+
+  phone:{
+    type:String,
+    required:true
+  },
+
+  gender:{
+    type:String,
+    required:true
+  },
+
+  dob:{
+    type:String,
+    required:true
+  },
+
+  photo:{
+    url:{
+      type:String
+    },
+
+    publicID:{
+      type:String
+    }
+  }
+},
+{
+  timestamps:true
+}
 );
-const User = mongoose.model(UserSchema, "User");
+
+const User = mongoose.model("User", UserSchema);
+
 export default User;
