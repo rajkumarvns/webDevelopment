027ea627bfdf470e5587e25dc59b532823423106
rajkumarvns <rajkumar966601@gmail.com>
2026-06-25 21:11:32 +0530

    feat: improve error handling in registerUser and update response messages

--- BackEnd/practiceBackend/index.js
+++ BackEnd/practiceBackend/index.js
@@ -5,21 +5,32 @@ import express from "express";
 import AuthRouter from "./src/routers/auth.route.js";
 import PublicRouter from "./src/routers/public.route.js";
 import connectDB from "./src/config/dbConnection.config.js";
+
 const app = express();
+
 app.use(express.json());
+
 app.use("/auth", AuthRouter);
 app.use("/public", PublicRouter);
 
+//Default API
 app.get("/", (req, res) => {
-  console.log("default get API Hit first");
-
-  res.json({
-    message: "welcome once again first",
-  });
+  console.log("Default Get API Hit");
+  res.json({ message: "Welcome to my first backend Project" });
 });
+
+//Default Error Handler
+
+app.use((err, req, res, next) => {
+  const ErrMessage = err.message || "Internal Server Error";
+  const ErrStausCode = err.statusCode || 500;
+
+  res.status(ErrStausCode).json({ message: ErrMessage });
+});
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log("server start on port", port);
+  console.log("Server Started on port:", port);
   connectDB();
 });
--- BackEnd/practiceBackend/src/controllers/auth.controller.js
+++ BackEnd/practiceBackend/src/controllers/auth.controller.js
@@ -5,22 +5,42 @@ export const loginUser = (req, res) => {
 export const logoutUser = (req, res) => {
   res.json({ message: "logout User successfull from controller" });
 };
-export const registerUser = async (req, res) => {
+export const registerUser = async (req, res, next) => {
   // res.json({ message: "register user successfull from controller" });
   try {
     const { fullName, email, password, phone, gender, dob } = req.body;
     if (!fullName || !email || !password || !phone || !gender || !dob) {
-      res.status(400).json({ message: "All field required" });
-      return;
+      const error = new Error("All fields Required");
+      error.statusCode = 400;
+      return next(error);
     }
+
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      res.status(409).json({ message: "email already registered" });
-      return;
+      const error = new Error("Email Already Registred");
+      error.statusCode = 409;
+      return next(error);
     }
-  } catch (error) {
-    res.status(500).json({
-      message: "Internal Server Error",
+    const photoUrl = `https://placehold.co/600x400?text=${fullName.charAt(0).toUpperCase()}`;
+
+    const photo = {
+      url: photoUrl,
+      publicId: null,
+    };
+
+    const newUser = await User.create({
+      fullName,
+      email,
+      password,
+      phone,
+      gender,
+      dob,
+      photo,
     });
+
+    res.status(201).json({ message: "User created Successfully" });
+  } catch (error) {
+    console.log(error.message);
+    next();
   }
 };
